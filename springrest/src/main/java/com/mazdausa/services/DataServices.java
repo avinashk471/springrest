0b386715dfedcce2b3f539c613396666f35b6946
Avinash <avinashk471@yahoo.com>
2017-10-26 11:03:01 -0700

    Initial Commit

---
+++ springrest/src/main/java/com/mazdausa/services/DataServices.java
@@ -0,0 +1,15 @@
+package com.mazdausa.services;
+
+import java.util.List;
+
+import com.mazdausa.model.Employee;
+
+public interface DataServices {
+	public boolean addEntity(Employee employee) throws Exception;
+
+	public Employee getEntityById(long id) throws Exception;
+
+	public List<Employee> getEntityList() throws Exception;
+
+	public boolean deleteEntity(long id) throws Exception;
+}
